c6ab32b85231ab5e6edbfb9ac68af62812bd040d
MIDHUNAVATHAR <midhun201819@gmail.com>
2025-05-24 14:25:41 +0530

    Initial commit - Netflix clone

---
+++ src/assets/cards/Cards_data.js
@@ -0,0 +1,67 @@
+import card_img1 from './card1.png'
+import card_img2 from './card2.png'
+import card_img3 from './card3.png'
+import card_img4 from './card4.png'
+import card_img5 from './card5.png'
+import card_img6 from './card6.png'
+import card_img7 from './card7.png'
+import card_img8 from './card8.png'
+import card_img9 from './card9.png'
+import card_img10 from './card10.png'
+import card_img11 from './card11.png'
+import card_img12 from './card12.png'
+
+
+const cards_data = [
+    {
+        image:card_img1,
+        name:"Kung Fu Panda"
+    },
+    {
+        image:card_img2,
+        name:'Squid Challenge'
+    },
+    {
+        image:card_img3,
+        name:"Squid Challenge"
+    },
+    {
+        image:card_img4,
+        name:"Jawan"
+    },
+    {
+        image:card_img5,
+        name:"Mahaan"
+    },
+    {
+        image:card_img6,
+        name:"Trans"
+    },
+    {
+        image:card_img7,
+        name:"Robin Hood"
+    },
+    {
+        image:card_img8,
+        name:"Good Bad Ugly"
+    },
+    {
+        image:card_img9,
+        name:"Final Destination"
+    },
+    {
+        image:card_img10,
+        name:'Bromance'
+    },
+    {
+        image:card_img11,
+        name:'18 Pages'
+    },
+    {
+        image:card_img12,
+        name:'Little Hearts'
+    }
+]
+
+
+export default cards_data; 
